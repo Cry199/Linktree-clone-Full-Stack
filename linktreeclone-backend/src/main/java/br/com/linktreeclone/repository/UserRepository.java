73f5540d2de301c3364741c6199fa9400a5d9bc7
Cauã <caualeu2016@gmail.com>
2025-09-14 17:25:29 -0300

    adiciona método para buscar usuário por email no UserRepository

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/repository/UserRepository.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/repository/UserRepository.java
@@ -4,7 +4,11 @@ import br.com.linktreeclone.entity.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> { }
+public interface UserRepository extends JpaRepository<User, UUID>
+{
+    Optional<User> findByEmail(String email);
+}
