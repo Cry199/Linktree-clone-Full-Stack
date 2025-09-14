7ba697d703f449c7e8c162052c754df89d4a113d
Cauã <caualeu2016@gmail.com>
2025-09-14 16:55:56 -0300

    adiciona a entidade User com mapeamento JPA e atributos de username e password

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/entity/User.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/entity/User.java
@@ -1,4 +1,22 @@
 package br.com.linktreeclone.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
 }
