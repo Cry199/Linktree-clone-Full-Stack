3c8f04a13edfb5387fc2948675a9ac715a3ff130
Cauã <caualeu2016@gmail.com>
2025-09-14 17:03:59 -0300

    atualiza o repositório UserRepository para usar UUID como identificador

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/repository/UserRepository.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/repository/UserRepository.java
@@ -4,5 +4,7 @@ import br.com.linktreeclone.entity.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
+public interface UserRepository extends JpaRepository<User, UUID> { }
