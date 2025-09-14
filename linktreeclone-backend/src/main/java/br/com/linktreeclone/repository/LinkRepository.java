93c82a9bff526fcce93307ba3cb427975a767c1f
Cauã <caualeu2016@gmail.com>
2025-09-14 18:18:59 -0300

    adiciona a interface LinkRepository estendendo JpaRepository e implementa método para buscar links por usuário

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/repository/LinkRepository.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/repository/LinkRepository.java
@@ -1,4 +1,13 @@
 package br.com.linktreeclone.repository;
 
-public interface LinkRepository {
+import br.com.linktreeclone.entity.Link;
+import br.com.linktreeclone.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface LinkRepository extends JpaRepository<Link, UUID>
+{
+    List<Link> findAllByUser(User user);
 }
