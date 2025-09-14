cee97c3112398c267889d536266cd080b822e28e
Cauã <caualeu2016@gmail.com>
2025-09-14 17:03:52 -0300

    atualiza a entidade User para usar UUID como identificador e adiciona validação de email

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/entity/User.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/entity/User.java
@@ -1,8 +1,11 @@
 package br.com.linktreeclone.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
@@ -11,11 +14,16 @@ import lombok.Setter;
 public class User
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
 
     @Column(nullable = false, unique = true)
     private String username;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
 
     @Column(nullable = false)
     private String password;
