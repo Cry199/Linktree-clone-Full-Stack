aa9c42c2bfc6d2d6fa0b2c59fefe117edaed36fd
Cauã <caualeu2016@gmail.com>
2025-09-14 17:39:02 -0300

    adiciona endpoint para obter perfil do usuário autenticado

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/controller/UserController.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/controller/UserController.java
@@ -5,10 +5,9 @@ import br.com.linktreeclone.entity.User;
 import br.com.linktreeclone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -22,6 +21,14 @@ public class UserController
     {
         User createdUser = userService.createUser(user);
         return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getMyProfile()
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
     }
 
 }
