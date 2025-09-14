7251b93dfb6d4a593d0fbfdcc65a2e083df5275f
Cauã <caualeu2016@gmail.com>
2025-09-14 18:18:46 -0300

    adiciona a classe Link com campos id, title, url e relacionamento com User

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/entity/Link.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/entity/Link.java
@@ -1,4 +1,31 @@
 package br.com.linktreeclone.entity;
 
-public class Link {
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "links")
+@Getter
+@Setter
+public class Link
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private User user;
 }
