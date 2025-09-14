f226ac6951aa67f04c98510a4d4bc49930293b40
Cauã <caualeu2016@gmail.com>
2025-09-14 19:48:37 -0300

    adiciona tratamento de exceções com respostas personalizadas para ResourceNotFoundException e UnauthorizedException

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/exception/RestExceptionHandler.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/exception/RestExceptionHandler.java
@@ -1,4 +1,41 @@
 package br.com.linktreeclone.exception;
 
-public class RestExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler
+{
+    public record ErrorResponse(LocalDateTime timestamp, int status, String error, String message, String path) {}
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", "")
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.FORBIDDEN.value(),
+                "Forbidden",
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", "")
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+    }
 }
