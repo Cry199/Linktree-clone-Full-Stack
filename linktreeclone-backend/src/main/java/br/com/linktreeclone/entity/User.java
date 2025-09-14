c9a433e882d5b606b4b746475a194472247fe730
Cauã <caualeu2016@gmail.com>
2025-09-14 17:39:21 -0300

    implementa UserDetails na classe User para gerenciamento de segurança

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/entity/User.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/entity/User.java
@@ -1,17 +1,23 @@
 package br.com.linktreeclone.entity;
 
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class User
+public class User implements UserDetails
 {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -27,4 +33,29 @@ public class User
 
     @Column(nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
