b2889cab0f076e3b8942e58a4b1002007a80f5e8
Cauã <caualeu2016@gmail.com>
2025-09-14 18:18:37 -0300

    adiciona campos title e url à classe LinkRequestDTO e implementa LinkResponseDTO com construtor baseado em Link

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/dto/LinkResponseDTO.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/dto/LinkResponseDTO.java
@@ -1,4 +1,13 @@
 package br.com.linktreeclone.dto;
 
-public record LinkResponseDTO() {
+import br.com.linktreeclone.entity.Link;
+
+import java.util.UUID;
+
+public record LinkResponseDTO(UUID id, String title, String url)
+{
+    public LinkResponseDTO(Link link)
+    {
+        this(link.getId(), link.getTitle(), link.getUrl());
+    }
 }
