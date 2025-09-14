c83b1aa5526ddd5e25048dfcafa5c33851f75ba8
Cauã <caualeu2016@gmail.com>
2025-09-14 16:55:46 -0300

    adiciona endpoint de registro de usuários no UserController

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/controller/UserController.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/controller/UserController.java
@@ -1,4 +1,27 @@
 package br.com.linktreeclone.controller;
 
-public class UserController {
+import br.com.linktreeclone.entity.User;
+
+import br.com.linktreeclone.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController
+{
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user)
+    {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
 }
