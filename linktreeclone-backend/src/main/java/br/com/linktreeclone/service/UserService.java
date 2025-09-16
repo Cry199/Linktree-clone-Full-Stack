273641b626b04870803d188ce6101b3380763743
Cauã <caualeu2016@gmail.com>
2025-09-16 18:22:19 -0300

    Testando o workflow de CI/CD

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/service/UserService.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/service/UserService.java
@@ -20,6 +20,5 @@ public class UserService
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
-
-    // Testando o workflow de CI/CD
+    
 }
