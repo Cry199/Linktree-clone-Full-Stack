5e197c63be336dc788a3323fee07e121571d741b
Cauã <caualeu2016@gmail.com>
2025-09-16 21:14:46 -0300

    Web Config

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/config/WebConfig.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/config/WebConfig.java
@@ -1,5 +1,6 @@
 package br.com.linktreeclone.config;
 
+<<<<<<< HEAD
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,3 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*");
     }
 }
+=======
+public class WebConfig {
+}
+>>>>>>> 4f68eb6 (Web Config)
