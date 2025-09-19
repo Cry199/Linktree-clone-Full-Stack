c01485267ef0b2ed116c82af6251ee22e034e615
Cauã <caualeu2016@gmail.com>
2025-09-19 19:10:30 -0300

    Adds public profile API endpoint
    Introduces a new REST endpoint to fetch a user's public profile and links by username. This enables unauthenticated access to shared profiles, which is a core feature for the Linktree clone.

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/controller/PublicController.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/controller/PublicController.java
@@ -1,4 +1,23 @@
 package br.com.linktreeclone.controller;
 
-public class PublicController {
+import br.com.linktreeclone.dto.PublicProfileDTO;
+import br.com.linktreeclone.service.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PublicController
+{
+    @Autowired
+    private LinkService linkService;
+
+    @GetMapping("/{username}")
+    public ResponseEntity<PublicProfileDTO> getPublicProfile(@PathVariable String username) {
+        PublicProfileDTO publicProfile = linkService.getPublicProfileByUsername(username);
+        return ResponseEntity.ok(publicProfile);
+    }
 }
