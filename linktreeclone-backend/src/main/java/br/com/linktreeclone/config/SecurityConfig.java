3f20b8968c8dc6176f60ac834800f1741a79e2e4
Cauã <caualeu2016@gmail.com>
2025-09-19 19:10:39 -0300

    Adds public access for user profiles
    Allows unauthenticated users to view public user profiles via GET requests to the `/{username}` endpoint. This change enables the core functionality of a link tree, where profiles are meant to be publicly accessible.

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/config/SecurityConfig.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import br.com.linktreeclone.security.SecurityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,7 +25,11 @@ public class SecurityConfig
     {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/register", "/auth/login").permitAll()
+                        .requestMatchers(
+                                "/users/register",
+                                "/auth/login"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/{username}").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf.disable())
