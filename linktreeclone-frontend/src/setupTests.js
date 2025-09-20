b47673569fb1e7d5413ea75b731849658746c737
Cauã <caualeu2016@gmail.com>
2025-09-20 02:19:51 -0300

    Corrige comentário para garantir clareza na função de limpeza após os testes

--- linktreeclone-frontend/src/setupTests.js
+++ linktreeclone-frontend/src/setupTests.js
@@ -3,7 +3,7 @@ import { cleanup } from '@testing-library/react';
 
 import '@testing-library/jest-dom/vitest';
 
-// Garante que a limpeza seja executada após cada teste
+// Garante que a limpeza seja executada após cada testes
 afterEach(() => {
   cleanup();
 });
