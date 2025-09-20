9aedd22973bac915db89924f418549c3bcbde3d5
Cauã <caualeu2016@gmail.com>
2025-09-20 00:49:23 -0300

    Remove valores padrão de email e senha no LoginPage

--- linktreeclone-frontend/src/features/auth/pages/LoginPage.jsx
+++ linktreeclone-frontend/src/features/auth/pages/LoginPage.jsx
@@ -4,8 +4,8 @@ import { useAuth } from '../../../context/AuthContext';
 import './AuthForm.css';
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('Bento@Bento.com'); 
-  const [password, setPassword] = useState('123');  
+  const [email, setEmail] = useState(''); 
+  const [password, setPassword] = useState('');  
   const [error, setError] = useState('');
   const navigate = useNavigate();
   const { login } = useAuth();
