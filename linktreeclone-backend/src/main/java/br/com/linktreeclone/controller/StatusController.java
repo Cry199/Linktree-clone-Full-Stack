0e38926501836a70382fc4176b50a4e894267a67
Cauã <caualeu2016@gmail.com>
2025-09-20 01:43:31 -0300

    Remove código não utilizado e organize as importações na classe StatusController

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/controller/StatusController.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/controller/StatusController.java
@@ -1,4 +1,17 @@
 package br.com.linktreeclone.controller;
 
-public class StatusController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class StatusController
+{
+    @GetMapping("/status")
+    public ResponseEntity<Map<String, String>> getApiStatus() {
+        Map<String, String> status = Map.of("status", "ok", "message", "API is running");
+        return ResponseEntity.ok(status);
+    }
 }
