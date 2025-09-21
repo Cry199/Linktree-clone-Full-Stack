fd8d7e7e108c39d13e6dbd4809281d72fdfb592c
Cauã <caualeu2016@gmail.com>
2025-09-21 15:14:21 -0300

    refactor(exception): ajusta a formatação da classe InvalidTokenException

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/exception/InvalidTokenException.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/exception/InvalidTokenException.java
@@ -1,7 +1,8 @@
 package br.com.linktreeclone.exception;
 
-public class InvalidTokenException extends RuntimeException {
-  public InvalidTokenException(String message) {
-    super(message);
-  }
+public class InvalidTokenException extends RuntimeException
+{
+    public InvalidTokenException(String message) {
+        super(message);
+    }
 }
