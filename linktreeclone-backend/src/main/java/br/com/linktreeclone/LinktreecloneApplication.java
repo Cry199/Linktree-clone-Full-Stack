de63d8bf91b3d51887a155e95cd8c43b3cb91314
Cauã Ribeiro <56047994+Cry199@users.noreply.github.com>
2025-09-28 22:27:22 -0300

    Format main method for better readability

--- linktreeclone-backend/src/main/java/br/com/linktreeclone/LinktreecloneApplication.java
+++ linktreeclone-backend/src/main/java/br/com/linktreeclone/LinktreecloneApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LinktreecloneApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(LinktreecloneApplication.class, args);
 	}
 }
